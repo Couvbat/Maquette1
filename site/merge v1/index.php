2567b859bb3e681e3ee2726d2a188e3c75c802db
Couvbat <mrcouvbat@gmail.com>
2021-10-16 18:31:39 +0200

    Create index.php

---
+++ site/merge v1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="rsc/style.css">
+    <link rel="icon" type="image/png" href="rsc/ico.png">
+    <title>DS Restaurant</title>
+</head>
+<body>
+    <?php
+        require "inc/header.html";
+        require "inc/part1.html";
+        require "inc/part2.html";
+        require "inc/part3.html";
+        require "inc/part4.html";
+        require "inc/part5.html";
+        require "inc/part6.html";
+        require "inc/part7.html";
+        require "inc/footer.html";
+    ?>
+</body>
+</html>
